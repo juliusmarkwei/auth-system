44d19ace06331cf6cafcc0bcb80e05770d3a4434
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-18 06:15:57 +0000

    url for token auth

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -22,9 +22,8 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('admin/', admin.site.urls),
-    path('user/', include('users.urls', namespace='users')),
-
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("admin/", admin.site.urls),
+    path("user/", include("users.urls", namespace="users")),
 ]
