e1d77a3da5329ddb11e7ba7346371d3c00973938
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-19 12:04:52 +0000

    removed password

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,5 +3,22 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User, UserTokens
 
 
-admin.site.register(User)
-admin.site.register(UserTokens)
+# admin.site.register(User)
+# admin.site.register(UserTokens)
+
+
+@admin.register(User)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = [
+        "first_name",
+        "last_name",
+        "email",
+        "is_staff",
+        "telephone",
+        "date_joined",
+    ]
+
+
+@admin.register(UserTokens)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["email", "token", "last_updated"]
