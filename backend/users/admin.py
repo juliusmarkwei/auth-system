bd7be3ed48d9caffda7b9679136900324cdaaf7d
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-19 09:51:48 +0000

    registered usertokens

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, UserTokens
 
 
 admin.site.register(User)
+admin.site.register(UserTokens)
