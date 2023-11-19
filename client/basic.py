041d039efae301b17eb7d82a51f2935624c863f8
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-19 12:24:30 +0000

    py client

--- client/basic.py
+++ client/basic.py
@@ -1,8 +1,10 @@
 import requests
 
 
-endpoint = "http://127.0.0.1:8000/api/"
+endpoint = "http://127.0.0.1:8000/admin/"
 
-response = requests.get(endpoint, params={"abc": 123}, json={'message': 'Julius Markwei'})
+response = requests.get(
+    endpoint, params={"abc": 123}, json={"message": "Julius Markwei"}
+)
 print(response)
 print(response.status_code)
