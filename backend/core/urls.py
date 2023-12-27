1cc0e843fbbbb6680d783ec847a7f0ebe008b01c
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-27 23:16:23 +0000

    core url

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.views import (
 )
 from rest_framework.documentation import include_docs_urls
 
+
 urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
