8bd7d6f8e29cd5c9e28ca0d842369c0ef33a6f4a
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-28 19:25:40 +0000

    added email and user info urls paths

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import UserAPIView, UserRetrieveUpdateAPIView, authenticate_user
+from .views import UserAPIView, SendEmailConfirmationTokenAPIView, UserInformationAPIView
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("create/", UserAPIView.as_view()),
-    path("update/", UserRetrieveUpdateAPIView.as_view()),
-    path("obtain_token/", authenticate_user),
+    path("", UserAPIView.as_view()),
+    path("me/", UserInformationAPIView.as_view(), name="user_information_api_view"),
+    path("send-confirmation-email/", SendEmailConfirmationTokenAPIView.as_view(), name="send_confirmation_email"),
 ]
