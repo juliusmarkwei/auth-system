c29baa0d9bd51cb23593aa1d5c49257f826465c2
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-28 17:10:56 +0000

    verifying url config

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -10,7 +10,10 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+   
     path("admin/", admin.site.urls),
+    
     path("accounts/", include("accounts.urls", namespace="accounts")),
+    
     path("", include_docs_urls(title="Authentication System's Docs")),
 ]
