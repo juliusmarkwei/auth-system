088b02d74abffc8a64877d1b66070b391af9c480
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-29 16:58:02 +0000

    added password validator

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -22,3 +22,15 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
         extra_kwargs = {"password": {"write_only": True}}
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        
+        # Adding the below line made it work for me.
+        instance.is_active = True
+        if password is not None:
+            # Set password does the hash, so you don't need to call make_password 
+            instance.set_password(password)
+        instance.save()
+        return instance
