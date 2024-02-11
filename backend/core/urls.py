2c23df030431a85c6d4a8081669e7f8e4d55572b
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-11 23:53:42 +0000

    added djoser auth endpoints

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -9,8 +9,9 @@ from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path("api-auth/", include("rest_framework.urls")), #default drf authentincation
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls", namespace="accounts")),
     path("", include_docs_urls(title="Authentication System's Docs")),
