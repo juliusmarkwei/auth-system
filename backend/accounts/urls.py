54600dcde0fc00c9f7a0fbb38b9992213f735375
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-12 04:12:51 +0000

    removed reduntant paths

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import UserAPIView, SendEmailConfirmationTokenAPIView, UserInformationAPIView
+from .views import UserAPIView, SendEmailConfirmationTokenAPIView
 
 app_name = "accounts"
 
 urlpatterns = [
     path("", UserAPIView.as_view()),
-    path("me/", UserInformationAPIView.as_view(), name="user_information_api_view"),
-    path("send-confirmation-email/", SendEmailConfirmationTokenAPIView.as_view(), name="send_confirmation_email"),
+    path("send-confirmation-email", SendEmailConfirmationTokenAPIView.as_view(), name="send_confirmation_email"),
 ]
