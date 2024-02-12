783b4934515d0f807f17c4d57741fabc8990fa9b
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-12 04:13:31 +0000

    removed duplicated views

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -8,15 +8,7 @@ from .utils import send_confirmation_email
 
 
 class UserAPIView(APIView):
-    permission_classes = [AllowAny,]
-
-    def post(self, request):
-        user = request.data
-        serializer = UserSerializer(data=user)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    permission_classes = [IsAuthenticated,]
 
     def get_queryset(self):
         return User.objects.all()
@@ -27,24 +19,13 @@ class UserAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, *args, **kwargs):
-        serializer_data = request.data.get("user", {})
-        serializer = UserSerializer(request.user, data=serializer_data, partial=True)
+        data = request.data
+        serializer = UserSerializer(request.user, data=data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
-class UserInformationAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-    
-    def get(self, request, *args, **kwargs):
-        user = request.user
-        email = user.email
-        is_verified = user.is_verified
-        
-        payload = {"email": email, "is_verified": is_verified}
-        return Response(data=payload, status=status.HTTP_200_OK)
     
     
 class SendEmailConfirmationTokenAPIView(APIView):
