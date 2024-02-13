97807c62e42c4539b93f9fe375916f19a94a88e3
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-13 23:24:21 +0000

    added description for docs

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
+from rest_framework.permissions import AllowAny
 
 
 admin.site.site_herder = "Auth System Admin Panel"
 admin.site.index_title = "Admin"
+
+schema_info = {
+    
+}
 
 urlpatterns = [
     path("api-auth/", include("rest_framework.urls")), #default drf authentincation
@@ -13,5 +18,13 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls", namespace="accounts")),
-    path("docs/", include_docs_urls(title="Authentication System's Docs")),
+    path("docs/", include_docs_urls(
+        title = "Authentication System's Docs",
+        description = """
+                        A simple authentication system build be extending the functionalities\
+                            of the Djoser library. This project utilizes allthe nasic CRUD \
+                                operation.
+                      """,
+        permission_classes = [AllowAny],
+    )),
 ]
