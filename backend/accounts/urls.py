4cb370d8e4ae65ddbdd96bd2894e5a5b1076816c
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-15 23:13:36 +0000

    accounts urls

--- backend/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserAPIView, SendEmailConfirmationTokenAPIView
-
-app_name = "accounts"
-
-urlpatterns = [
-    path("", UserAPIView.as_view()),
-    path("send-confirmation-email", SendEmailConfirmationTokenAPIView.as_view(), name="send_confirmation_email"),
-]
