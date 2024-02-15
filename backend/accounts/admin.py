d1e00eb1609aa699233e2e74bcbc70e29cc8d5e3
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-15 23:07:03 +0000

    admin file

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,25 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, EmailConfirmationToken
 
-
-@admin.register(User)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = [
-        "id",
-        "username",
-        "is_verified",
-        "first_name",
-        "last_name",
-        "email",
-        "is_staff",
-        "phone",
-        "date_joined",
-        "updated_at",
-    ]
-
-
-@admin.register(EmailConfirmationToken)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ["user", "created_at", "updated_at"]
-    
+# Register your models here.
