c1e162d3a9acfc632c4f6053b499a3265cb355af
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-17 07:05:18 +0000

    actions for dir

---
+++ frontend/src/actions/types.jsx
@@ -0,0 +1,4 @@
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const USER_LOADED_SUCCESS = "LOAD_USER_SUCCESS";
+export const USER_LOADED_FAIL = "LOAD_USER_FAIL";
