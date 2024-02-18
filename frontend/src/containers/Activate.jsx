d6abe0898a8a8d8cb4ad91731a5a89c6732a4062
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-18 03:23:48 +0000

    removed frontend

--- frontend/src/containers/Activate.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Activate = () => {
-  return (
-    <div>Activate</div>
-  );
-};
-
-export default Activate;
--- frontend/src/containers/ResetPassword.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ResetPassword = () => {
-  return (
-    <div>ResetPassword</div>
-  );
-};
-
-export default ResetPassword;
