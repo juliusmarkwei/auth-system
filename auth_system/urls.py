7ce0f236c40c7cf0fd17eacbdbe286fe93f6d2bc
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-18 03:26:47 +0000

    core urls

---
+++ auth_system/urls.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Authentication System",
+      default_version='v1',
+      description="Create and manage users",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="juliusmarkwei2000@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.social.urls')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
