0ff0713d5e0354ad7d988ec000dc429bcbe43008
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-19 16:04:05 +0000

    usercreate and user

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,10 +1,17 @@
-from djoser.serializers import UserCreateSerializer
+from djoser import serializers
 from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
 
-class UserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
+class UserCreateSerializer(serializers.UserCreateSerializer):
+    class Meta(serializers.UserCreateSerializer.Meta):
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'password']
+        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'password', 're_password', 'phone']
+        
+        
+class UserSerializer(serializers.UserSerializer):
+    class Meta(serializers.UserSerializer.Meta):
+        model = User
+        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'phone']
+
