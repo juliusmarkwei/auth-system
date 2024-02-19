01f5a0a356ff1daa50cb397104640f2c89be6824
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-19 16:04:25 +0000

    core urls

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
+    path('', include('accounts.urls')),
 ]
