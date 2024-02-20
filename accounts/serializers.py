6ecd056648d3340e4b6073c272d12b51daea35ac
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-20 02:44:53 +0000

    included new field

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,11 +7,11 @@ User = get_user_model()
 class UserCreateSerializer(serializers.UserCreateSerializer):
     class Meta(serializers.UserCreateSerializer.Meta):
         model = User
-        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'password', 're_password', 'phone']
+        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'password', 're_password', 'phone', 'is_active', 'is_phone_verified']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            're_password': {'write_only': True},
+            'is_active': {'read_only': True},
+            'is_phone_verified': {'read_only': True},
+        }
         
-        
-class UserSerializer(serializers.UserSerializer):
-    class Meta(serializers.UserSerializer.Meta):
-        model = User
-        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'phone']
-
