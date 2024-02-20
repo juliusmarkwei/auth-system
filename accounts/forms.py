d86dd2471fcc061d2586081f3dfb349b6f482c7a
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-20 02:43:56 +0000

    forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,8 +4,10 @@ from django import forms
 class UserCreateForm(forms.Form):
     username = forms.CharField(max_length=100)
     email = forms.EmailField(required=True)
-    password = forms.PasswordInput()
-    re_password = forms.PasswordInput()
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    re_password = forms.CharField(
+        label="Password confirm", widget=forms.PasswordInput
+    )
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
     phone = forms.CharField(max_length=100)
