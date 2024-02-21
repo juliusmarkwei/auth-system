a256c8b0e8e97e098f0a1f10a4909dbf73452bea
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-21 02:09:16 +0000

    added accounts urls

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.social.urls')),
+    
+    path('accounts/', include('accounts.urls')),
+    
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
