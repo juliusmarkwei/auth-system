028f718bbb614b0a8a7b2875b857e3cc3227bc20
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-21 02:07:43 +0000

    added group manager endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,9 @@
-from . import views
+from .views import GroupView, ManageGroup
 from django.urls import path
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('home', views.home, name='home'),
-    path('signin', views.signin, name='signin'),
-    path('auth/users/', views.signup),
+    path('group', GroupView.as_view(), name='group-view'),
+    path('manage-group', ManageGroup.as_view(), name='manage-group'),
 ]
