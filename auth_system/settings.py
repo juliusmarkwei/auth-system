701eb98362f613c43b3ea22cf34c71fc3bdc554e
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-21 02:09:45 +0000

    settings file

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -151,9 +151,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.UserAccount'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
