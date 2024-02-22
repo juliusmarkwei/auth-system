ab282d816a8fa9bc6daf931d8101583451a9f278
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-22 23:10:55 +0000

    fixed staticfiled root

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
