f9ab36682b2dfd3fde6f9a23111a45d873efa26b
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-22 23:07:31 +0000

    removed media path

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -33,5 +33,4 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
